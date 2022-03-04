fbc0502f5266951dd42d48bfc86efce558eac304
feitian124 <feitian124@gmail.com>
2022-03-05 06:54:32 +0800

    index page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,16 @@
 <template>
-  <h2 class="!mt-0">{{ props.msg }}</h2>
-  <button
-    class="px-3 py-2 bg-white border border-gray-300 rounded-md shadow"
-    @click="increment"
-  >count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>src/components/HelloWorld.vue</code> to test hot module
-    replacement.
-  </p>
+  <div class="text-center">
+    <h2 class="!mt-0">{{ props.msg }}</h2>
+    <button
+      class="px-3 py-2 bg-white border border-gray-300 rounded-md shadow"
+      @click="increment"
+    >count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>src/components/HelloWorld.vue</code> to test hot module
+      replacement.
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts">
